9de3ef53640e31e6eeae0b6746423859774a155f
sausage303 <parinya24dev@gmail.com>
2025-05-26 08:41:02 +0700

    fix: update API calls to use relative paths instead of BASE_URL

--- src/app/admission/form/[id]/service.tsx
+++ src/app/admission/form/[id]/service.tsx
@@ -1,5 +1,3 @@
-"use server";
-
 import constant from "@/constant";
 import { getFullName } from "@/lib/utils";
 import admission from "@/schema/admission";
@@ -27,14 +25,14 @@ export default async (
         formData.append("mimeType", "image/jpeg");
         formData.append("parentId", constant.folder.studentPhoto);
         formData.append("file", files.studentPhoto);
-        const { data: { id: idStudentPhoto } } = await axios.post(`${process.env.BASE_URL}/api/drive`, formData);
+        const { data: { id: idStudentPhoto } } = await axios.post("/api/drive", formData);
 
         let idHouseRecord;
 
         if (files.houseRecord) {
             formData.set("parentId", constant.folder.houseRecord);
             formData.set("file", files.houseRecord);
-            const { data: { id } } = await axios.post(`${process.env.BASE_URL}/api/drive`, formData);
+            const { data: { id } } = await axios.post("/api/drive", formData);
             idHouseRecord = id;
         }
 
@@ -43,7 +41,7 @@ export default async (
         if (files.studentRecord) {
             formData.set("parentId", constant.folder.studentRecord);
             formData.set("file", files.studentRecord);
-            const { data: { id } } = await axios.post(`${process.env.BASE_URL}/api/drive`, formData);
+            const { data: { id } } = await axios.post("/api/drive", formData);
             idStudentRecord = id;
         }
 
@@ -62,7 +60,7 @@ export default async (
                 year: "numeric"
             })
         }).forEach(([key, value]) => html = html.replaceAll(`{${key}}`, value));
-        const { data: file } = await axios.put(`${process.env.SERVER_API_URL}/html/pdf`, {
+        const { data: file } = await axios.put(`${process.env.NEXT_PUBLIC_SERVER_API_URL}/html/pdf`, {
             html,
             name: fullName
         }, { responseType: "blob" });
@@ -70,9 +68,9 @@ export default async (
         formData.set("mimeType", "application/pdf");
         formData.set("parentId", constant.folder.pdf);
         formData.set("file", file);
-        const { data: { id: idPdf } } = await axios.post(`${process.env.BASE_URL}/api/drive`, formData);
+        const { data: { id: idPdf } } = await axios.post("/api/drive", formData);
 
-        const { data: { success, message } } = await axios.post(`${process.env.BASE_URL}/api/data/admission`, {
+        const { data: { success, message } } = await axios.post("/api/data/admission", {
             ...data,
             studentPhoto: `https://drive.google.com/file/d/${idStudentPhoto}/preview`,
             houseRecord: idHouseRecord ? `https://drive.google.com/file/d/${idHouseRecord}/preview` : undefined,
