28fbaccbecb3b2a121b43693f64dbd35ae6a2756
sausage303 <parinya24dev@gmail.com>
2025-05-26 09:57:10 +0700

    fix: update metadata generation for admission form to include dynamic title

--- src/app/admission/form/[id]/page.tsx
+++ src/app/admission/form/[id]/page.tsx
@@ -1,10 +1,18 @@
 import { notFound } from "next/navigation";
 import Client from "./client"
 import { db } from "@/config";
+import admissionForm from "@/schema/admission-form";
 
-export const metadata = () => {
+export const generateMetadata = async ({ params }: { params: Promise<{ id: string }> }) => {
+    const id = (await params).id;
+    const exists = await db().admissionForm.get(id);
+
+    if (!exists) {
+        return notFound();
+    }
+
     return {
-        title: 'Admission Form'
+        title: `${admissionForm.typeView[exists.type]} ${admissionForm.roundView[exists.round]} ระดับชั้น ม.${exists.class}`
     }
 }
 
