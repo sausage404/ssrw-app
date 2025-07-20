f7227b2bb5a89bd33fac08b867f070fa0ad42de7
sausage303 <parinya24dev@gmail.com>
2025-07-20 15:48:23 +0700

    Implement code changes to enhance functionality and improve performance

--- database/backup.cmd
+++ database/backup.cmd
@@ -1,1 +1,5 @@
 docker run --rm -v pgdata:/data -v %cd%\backup:/backup alpine tar czf /backup/pgdata.tar.gz -C /data .
+
+@REM docker exec postgres_ssrw pg_dump -U myuser -d mydatabase -f /tmp/backup.sql
+
+@REM docker cp postgres_ssrw:/tmp/backup.sql ./backup.sql
