37437c071894d34fa28a77f0933078e9c096305c
Parinya Pantimit <parinya.pantimit@gmail.com>
2025-07-21 13:08:30 +0700

    Update page.tsx

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,6 +1,6 @@
 import { prisma } from "@/lib/prisma"
 import Client from "./client"
-
+export const dynamic = "force-dynamic";
 export default async () => {
     const user = await prisma.user.findMany({});
     return <Client user={user} />
