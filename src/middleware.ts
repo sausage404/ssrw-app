33a7317e6adba339c7e47e3c1017ecf15a284ddf
sausage404 <parinya.pantimit@gmail.com>
2025-09-27 15:12:03 +0700

    fix: update build script to include prisma generate and clean up middleware comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,17 +6,17 @@ const routes = ["/setting", "/announcement", "/attendance", "/club"];
 export async function middleware(request: NextRequest) {
     const isAuthenticated = await auth();
 
-    // if (!isAuthenticated && routes.some((route) => request.nextUrl.pathname.startsWith(route))) {
-    //     return NextResponse.redirect(new URL("/auth", request.url));
-    // }
+    if (!isAuthenticated && routes.some((route) => request.nextUrl.pathname.startsWith(route))) {
+        return NextResponse.redirect(new URL("/auth", request.url));
+    }
 
-    // if (isAuthenticated && request.nextUrl.pathname.startsWith("/auth")) {
-    //     return NextResponse.redirect(new URL("/", request.url));
-    // }
+    if (isAuthenticated && request.nextUrl.pathname.startsWith("/auth")) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
 
-    // if (isAuthenticated?.user.role !== "ADMIN" && request.nextUrl.pathname.startsWith("/admin")) {
-    //     return NextResponse.rewrite(new URL("/not-found", request.url));
-    // }
+    if (isAuthenticated?.user.role !== "ADMIN" && request.nextUrl.pathname.startsWith("/admin")) {
+        return NextResponse.rewrite(new URL("/not-found", request.url));
+    }
 
     return NextResponse.next();
 }
